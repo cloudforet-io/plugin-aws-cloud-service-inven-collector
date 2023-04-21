6c9ca31a5bb42f778441791a8b9147e010a265f4
jiyungSong <bluese@megazone.com>
2023-04-21 12:49:30 +0900

    etc: remove bin directory

--- bin/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Build a docker image
-docker build -t pyengine/aws-cloud-services . --no-cache
-
-docker tag pyengine/aws-cloud-services pyengine/aws-cloud-services:1.1
-#docker tag pyengine/aws-cloud-services spaceone/aws-cloud-services:1.1
--- bin/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-RELATIVE_DIR=`dirname "$0"`
-echo $RELATIVE_DIR
-
-. $RELATIVE_DIR/build.sh
-docker rm test_plugin
-docker run -v "$(pwd)":/opt --name test_plugin pyengine/aws-cloud-services:1.1
-# docker exec -ti test_plugin /bin/bash
