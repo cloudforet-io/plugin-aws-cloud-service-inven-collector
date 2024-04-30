7ce8f620752cb3c7ef8a24c1f12a6233b8debc9e
Sooyoung98 <sooyoungkim@mz.co.kr>
2024-04-30 19:03:39 +0900

    fix: opt out ec2 resources for stg testing purposes (again)
    Signed-off-by: Sooyoung98 <sooyoungkim@mz.co.kr>

--- src/spaceone/inventory/connector/aws_ec2_connector/schema/service_type.py
+++ src/spaceone/inventory/connector/aws_ec2_connector/schema/service_type.py
@@ -1,9 +1,17 @@
 import os
 from spaceone.inventory.libs.common_parser import *
 from spaceone.inventory.libs.schema.dynamic_widget import ChartWidget, CardWidget
-from spaceone.inventory.libs.schema.dynamic_field import TextDyField, SearchField, DateTimeDyField, EnumDyField
-from spaceone.inventory.libs.schema.resource import CloudServiceTypeResource, CloudServiceTypeResponse, \
-    CloudServiceTypeMeta
+from spaceone.inventory.libs.schema.dynamic_field import (
+    TextDyField,
+    SearchField,
+    DateTimeDyField,
+    EnumDyField,
+)
+from spaceone.inventory.libs.schema.resource import (
+    CloudServiceTypeResource,
+    CloudServiceTypeResponse,
+    CloudServiceTypeMeta,
+)
 from spaceone.inventory.conf.cloud_service_conf import *
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
@@ -11,40 +19,58 @@ current_dir = os.path.abspath(os.path.dirname(__file__))
 """
 SECURITY GROUP
 """
-sg_total_count_conf = os.path.join(current_dir, 'widget/sg_total_count.yaml')
-inbound_rule_total_count_conf = os.path.join(current_dir, 'widget/inbound_rule_total_count.yaml')
-outbound_rule_total_count_conf = os.path.join(current_dir, 'widget/outbound_rule_total_count.yaml')
-instance_total_count_conf = os.path.join(current_dir, 'widget/instance_total_count.yaml')
-sg_count_by_region_conf = os.path.join(current_dir, 'widget/sg_count_by_region.yaml')
-sg_count_by_account_conf = os.path.join(current_dir, 'widget/sg_count_by_account.yaml')
+sg_total_count_conf = os.path.join(current_dir, "widget/sg_total_count.yaml")
+inbound_rule_total_count_conf = os.path.join(
+    current_dir, "widget/inbound_rule_total_count.yaml"
+)
+outbound_rule_total_count_conf = os.path.join(
+    current_dir, "widget/outbound_rule_total_count.yaml"
+)
+instance_total_count_conf = os.path.join(
+    current_dir, "widget/instance_total_count.yaml"
+)
+sg_count_by_region_conf = os.path.join(current_dir, "widget/sg_count_by_region.yaml")
+sg_count_by_account_conf = os.path.join(current_dir, "widget/sg_count_by_account.yaml")
 
 cst_sg = CloudServiceTypeResource()
-cst_sg.name = 'SecurityGroup'
-cst_sg.provider = 'aws'
-cst_sg.group = 'EC2'
-cst_sg.labels = ['Compute', 'Security']
-cst_sg.service_code = 'AmazonEC2'
+cst_sg.name = "SecurityGroup"
+cst_sg.provider = "aws"
+cst_sg.group = "EC2"
+cst_sg.labels = ["Compute", "Security"]
+cst_sg.service_code = "AmazonEC2"
 cst_sg.tags = {
-    'spaceone:icon': f'{ASSET_URL}/Amazon-VPC_VPN-Gateway_dark-bg.svg',
+    "spaceone:icon": f"{ASSET_URL}/Amazon-VPC_VPN-Gateway_dark-bg.svg",
 }
 
 cst_sg._metadata = CloudServiceTypeMeta.set_meta(
     fields=[
-        TextDyField.data_source('ID', 'data.group_id'),
-        TextDyField.data_source('VPC ID', 'data.vpc_id'),
-        TextDyField.data_source('Description', 'data.description'),
-        TextDyField.data_source('AWS Account ID', 'account')
+        TextDyField.data_source("ID", "data.group_id"),
+        TextDyField.data_source("VPC ID", "data.vpc_id"),
+        TextDyField.data_source("Description", "data.description"),
+        TextDyField.data_source("AWS Account ID", "account"),
     ],
     search=[
-        SearchField.set(name='Security Group ID', key='data.group_id'),
-        SearchField.set(name='VPC ID', key='data.vpc_id'),
-        SearchField.set(name='Inbound Protocol', key='data.ip_permissions.protocol_display'),
-        SearchField.set(name='Inbound Port Rage', key='data.ip_permissions.port_display'),
-        SearchField.set(name='Inbound Source', key='data.ip_permissions.source_display'),
-        SearchField.set(name='Outbound Protocol', key='data.ip_permissions_egress.protocol_display'),
-        SearchField.set(name='Outbound Port Rage', key='data.ip_permissions_egress.port_display'),
-        SearchField.set(name='Outbound Source', key='data.ip_permissions_egress.source_display'),
-        SearchField.set(name='AWS Account ID', key='account')
+        SearchField.set(name="Security Group ID", key="data.group_id"),
+        SearchField.set(name="VPC ID", key="data.vpc_id"),
+        SearchField.set(
+            name="Inbound Protocol", key="data.ip_permissions.protocol_display"
+        ),
+        SearchField.set(
+            name="Inbound Port Rage", key="data.ip_permissions.port_display"
+        ),
+        SearchField.set(
+            name="Inbound Source", key="data.ip_permissions.source_display"
+        ),
+        SearchField.set(
+            name="Outbound Protocol", key="data.ip_permissions_egress.protocol_display"
+        ),
+        SearchField.set(
+            name="Outbound Port Rage", key="data.ip_permissions_egress.port_display"
+        ),
+        SearchField.set(
+            name="Outbound Source", key="data.ip_permissions_egress.source_display"
+        ),
+        SearchField.set(name="AWS Account ID", key="account"),
     ],
     widget=[
         CardWidget.set(**get_data_from_yaml(sg_total_count_conf)),
@@ -52,93 +78,104 @@ cst_sg._metadata = CloudServiceTypeMeta.set_meta(
         CardWidget.set(**get_data_from_yaml(outbound_rule_total_count_conf)),
         CardWidget.set(**get_data_from_yaml(instance_total_count_conf)),
         ChartWidget.set(**get_data_from_yaml(sg_count_by_region_conf)),
-        ChartWidget.set(**get_data_from_yaml(sg_count_by_account_conf))
-    ]
+        ChartWidget.set(**get_data_from_yaml(sg_count_by_account_conf)),
+    ],
 )
 
 """
 AMI
 """
-ami_total_count_conf = os.path.join(current_dir, 'widget/ami_total_count.yaml')
-ami_count_by_region_conf = os.path.join(current_dir, 'widget/ami_count_by_region.yaml')
-ami_count_by_account_conf = os.path.join(current_dir, 'widget/ami_count_by_account.yaml')
+ami_total_count_conf = os.path.join(current_dir, "widget/ami_total_count.yaml")
+ami_count_by_region_conf = os.path.join(current_dir, "widget/ami_count_by_region.yaml")
+ami_count_by_account_conf = os.path.join(
+    current_dir, "widget/ami_count_by_account.yaml"
+)
 
 cst_ami = CloudServiceTypeResource()
-cst_ami.name = 'AMI'
-cst_ami.provider = 'aws'
-cst_ami.group = 'EC2'
-cst_ami.labels = ['Compute']
-cst_ami.service_code = 'AmazonEC2'
+cst_ami.name = "AMI"
+cst_ami.provider = "aws"
+cst_ami.group = "EC2"
+cst_ami.labels = ["Compute"]
+cst_ami.service_code = "AmazonEC2"
 cst_ami.tags = {
-    'spaceone:icon': f'{ASSET_URL}/Amazon-AMI.svg',
+    "spaceone:icon": f"{ASSET_URL}/Amazon-AMI.svg",
 }
 
 cst_ami._metadata = CloudServiceTypeMeta.set_meta(
     fields=[
-        TextDyField.data_source('ID', 'data.image_id'),
-        TextDyField.data_source('Source', 'data.image_location'),
-        TextDyField.data_source('Owner', 'account'),
-        EnumDyField.data_source('Is Public', 'data.public', default_badge={
-            'indigo.500': ['true'], 'coral.600': ['false']
-        }),
-        EnumDyField.data_source('Status', 'data.state', default_state={
-            'safe': ['available'],
-            'warning': ['pending', 'transient', 'deregistered', 'invalid'],
-            'alert': ['error', 'failed']
-        }),
-        TextDyField.data_source('Platform', 'data.platform'),
-        TextDyField.data_source('Root Device Type', 'data.root_device_type'),
-        TextDyField.data_source('Virtualization', 'data.virtualization_type'),
-        TextDyField.data_source('Architecture', 'data.architecture', options={
-            'is_optional': True
-        }),
-        TextDyField.data_source('Virtualization Type', 'data.virtualization_type', options={
-            'is_optional': True
-        }),
-        TextDyField.data_source('Description', 'data.description', options={
-            'is_optional': True
-        }),
-        TextDyField.data_source('Root Device Name', 'data.root_device_name', options={
-            'is_optional': True
-        }),
-        TextDyField.data_source('Root Device Type', 'data.root_device_type', options={
-            'is_optional': True
-        }),
-        TextDyField.data_source('SR-IOV Net Support ', 'data.sriov_net_support', options={
-            'is_optional': True
-        }),
-        TextDyField.data_source('Hypervisor ', 'data.hypervisor', options={
-            'is_optional': True
-        }),
-        TextDyField.data_source('Platform Details ', 'data.platform_details', options={
-            'is_optional': True
-        }),
-        TextDyField.data_source('AWS Account ID', 'account', options={
-            'is_optional': True
-        })
+        TextDyField.data_source("ID", "data.image_id"),
+        TextDyField.data_source("Source", "data.image_location"),
+        TextDyField.data_source("Owner", "account"),
+        EnumDyField.data_source(
+            "Is Public",
+            "data.public",
+            default_badge={"indigo.500": ["true"], "coral.600": ["false"]},
+        ),
+        EnumDyField.data_source(
+            "Status",
+            "data.state",
+            default_state={
+                "safe": ["available"],
+                "warning": ["pending", "transient", "deregistered", "invalid"],
+                "alert": ["error", "failed"],
+            },
+        ),
+        TextDyField.data_source("Platform", "data.platform"),
+        TextDyField.data_source("Root Device Type", "data.root_device_type"),
+        TextDyField.data_source("Virtualization", "data.virtualization_type"),
+        TextDyField.data_source(
+            "Architecture", "data.architecture", options={"is_optional": True}
+        ),
+        TextDyField.data_source(
+            "Virtualization Type",
+            "data.virtualization_type",
+            options={"is_optional": True},
+        ),
+        TextDyField.data_source(
+            "Description", "data.description", options={"is_optional": True}
+        ),
+        TextDyField.data_source(
+            "Root Device Name", "data.root_device_name", options={"is_optional": True}
+        ),
+        TextDyField.data_source(
+            "Root Device Type", "data.root_device_type", options={"is_optional": True}
+        ),
+        TextDyField.data_source(
+            "SR-IOV Net Support ",
+            "data.sriov_net_support",
+            options={"is_optional": True},
+        ),
+        TextDyField.data_source(
+            "Hypervisor ", "data.hypervisor", options={"is_optional": True}
+        ),
+        TextDyField.data_source(
+            "Platform Details ", "data.platform_details", options={"is_optional": True}
+        ),
+        TextDyField.data_source(
+            "AWS Account ID", "account", options={"is_optional": True}
+        ),
     ],
     search=[
-        SearchField.set(name='AMI ID', key='data.image_id'),
-        SearchField.set(name='Status', key='data.state'),
-        SearchField.set(name='Source', key='data.image_location'),
-        SearchField.set(name='Is Public', key='data.public', data_type='boolean'),
-        SearchField.set(name='Platform', key='data.platform'),
-        SearchField.set(name='Root Device Type', key='data.root_device_type'),
-        SearchField.set(name='Virtualization', key='data.virtualization_type'),
-        SearchField.set(name='Instance ID', key='data.instances.instance_id'),
-        SearchField.set(name='Instance Name', key='data.instances.instance_name'),
-        SearchField.set(name='Instance State', key='data.instances.state'),
-        SearchField.set(name='AWS Account ID', key='account')
+        SearchField.set(name="AMI ID", key="data.image_id"),
+        SearchField.set(name="Status", key="data.state"),
+        SearchField.set(name="Source", key="data.image_location"),
+        SearchField.set(name="Is Public", key="data.public", data_type="boolean"),
+        SearchField.set(name="Platform", key="data.platform"),
+        SearchField.set(name="Root Device Type", key="data.root_device_type"),
+        SearchField.set(name="Virtualization", key="data.virtualization_type"),
+        SearchField.set(name="Instance ID", key="data.instances.instance_id"),
+        SearchField.set(name="Instance Name", key="data.instances.instance_name"),
+        SearchField.set(name="Instance State", key="data.instances.state"),
+        SearchField.set(name="AWS Account ID", key="account"),
     ],
     widget=[
         CardWidget.set(**get_data_from_yaml(ami_total_count_conf)),
         ChartWidget.set(**get_data_from_yaml(ami_count_by_region_conf)),
-        ChartWidget.set(**get_data_from_yaml(ami_count_by_account_conf))
-    ]
+        ChartWidget.set(**get_data_from_yaml(ami_count_by_account_conf)),
+    ],
 )
 
-
 CLOUD_SERVICE_TYPES = [
-    CloudServiceTypeResponse({'resource': cst_sg}),
-    CloudServiceTypeResponse({'resource': cst_ami}),
+    CloudServiceTypeResponse({"resource": cst_sg}),
+    CloudServiceTypeResponse({"resource": cst_ami}),
 ]
--- src/spaceone/inventory/connector/aws_s3_connector/schema/service_type.py
+++ src/spaceone/inventory/connector/aws_s3_connector/schema/service_type.py
@@ -1,97 +1,149 @@
 import os
 from spaceone.inventory.libs.common_parser import *
 from spaceone.inventory.libs.schema.dynamic_widget import ChartWidget, CardWidget
-from spaceone.inventory.libs.schema.dynamic_field import TextDyField, SearchField, EnumDyField, SizeField, ListDyField
-from spaceone.inventory.libs.schema.resource import CloudServiceTypeResource, CloudServiceTypeResponse, CloudServiceTypeMeta
+from spaceone.inventory.libs.schema.dynamic_field import (
+    TextDyField,
+    SearchField,
+    EnumDyField,
+    SizeField,
+    ListDyField,
+)
+from spaceone.inventory.libs.schema.resource import (
+    CloudServiceTypeResource,
+    CloudServiceTypeResponse,
+    CloudServiceTypeMeta,
+)
 from spaceone.inventory.conf.cloud_service_conf import *
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
 
-bucket_total_count_conf = os.path.join(current_dir, 'widget/bucket_total_count.yaml')
-object_total_count_conf = os.path.join(current_dir, 'widget/object_total_count.yaml')
-object_total_size_conf = os.path.join(current_dir, 'widget/object_total_size.yaml')
-bucket_count_by_region_conf = os.path.join(current_dir, 'widget/bucket_count_by_region.yaml')
-object_count_by_region_conf = os.path.join(current_dir, 'widget/object_count_by_region.yaml')
-object_total_size_by_region_conf = os.path.join(current_dir, 'widget/object_total_size_by_region.yaml')
-bucket_count_by_account_conf = os.path.join(current_dir, 'widget/bucket_count_by_account.yaml')
-object_count_by_account_conf = os.path.join(current_dir, 'widget/object_count_by_account.yaml')
-object_total_size_by_account_conf = os.path.join(current_dir, 'widget/object_total_size_by_account.yaml')
+bucket_total_count_conf = os.path.join(current_dir, "widget/bucket_total_count.yaml")
+object_total_count_conf = os.path.join(current_dir, "widget/object_total_count.yaml")
+object_total_size_conf = os.path.join(current_dir, "widget/object_total_size.yaml")
+bucket_count_by_region_conf = os.path.join(
+    current_dir, "widget/bucket_count_by_region.yaml"
+)
+object_count_by_region_conf = os.path.join(
+    current_dir, "widget/object_count_by_region.yaml"
+)
+object_total_size_by_region_conf = os.path.join(
+    current_dir, "widget/object_total_size_by_region.yaml"
+)
+bucket_count_by_account_conf = os.path.join(
+    current_dir, "widget/bucket_count_by_account.yaml"
+)
+object_count_by_account_conf = os.path.join(
+    current_dir, "widget/object_count_by_account.yaml"
+)
+object_total_size_by_account_conf = os.path.join(
+    current_dir, "widget/object_total_size_by_account.yaml"
+)
 
 cst_bucket = CloudServiceTypeResource()
-cst_bucket.name = 'Bucket'
-cst_bucket.provider = 'aws'
-cst_bucket.group = 'S3'
-cst_bucket.labels = ['Storage']
+cst_bucket.name = "Bucket"
+cst_bucket.provider = "aws"
+cst_bucket.group = "S3"
+cst_bucket.labels = ["Storage"]
 cst_bucket.is_primary = True
 cst_bucket.is_major = True
-cst_bucket.service_code = 'AmazonS3'
+cst_bucket.service_code = "AmazonS3"
 cst_bucket.tags = {
-    'spaceone:icon': f'{ASSET_URL}/Amazon-S3.svg',
+    "spaceone:icon": f"{ASSET_URL}/Amazon-S3.svg",
 }
 
 cst_bucket._metadata = CloudServiceTypeMeta.set_meta(
     fields=[
-        EnumDyField.data_source('Access', 'data.public_access', default_badge={
-            'indigo.500': ['Private'],
-            'coral.600': ['Public']
-        }),
-        TextDyField.data_source('Object Total Counts', 'data.object_count'),
-        SizeField.data_source('Object Total Size', 'instance_size'),
-        TextDyField.data_source('ARN', 'data.arn', options={
-            'is_optional': True
-        }),
-        TextDyField.data_source('Versioning Status', 'data.versioning.status', options={
-            'is_optional': True
-        }),
-        TextDyField.data_source('Webhosting Index Document', 'data.website_hosting.index_document', options={
-            'is_optional': True
-        }),
-        TextDyField.data_source('Webhosting Error Document', 'data.website_hosting.error_document', options={
-            'is_optional': True
-        }),
-        TextDyField.data_source('Webhosting Error Document', 'data.website_hosting.routing_rules', options={
-            'is_optional': True
-        }),
-        TextDyField.data_source('Access Logging Target Bucket', 'data.server_access_logging.target_bucket', options={
-            'is_optional': True
-        }),
-        TextDyField.data_source('Access Logging Target Prefix', 'data.server_access_logging.target_prefix', options={
-            'is_optional': True
-        }),
-        ListDyField.data_source('Webhosting Routing Rules Condition', 'data.website_hosting.routing_rules', options={
-            'sub_key': 'condition',
-            'delimiter': '<br>',
-            'is_optional': True
-        }),
-        TextDyField.data_source('Object Lock Enabled', 'data.object_lock.object_lock_enabled', options={
-            'is_optional': True
-        }),
-        TextDyField.data_source('Transfer Acceleration Enabled', 'data.transfer_acceleration.transfer_acceleration',
-                                options={'is_optional': True}),
-        ListDyField.data_source('Notification Type', 'data.notification_configurations', options={
-            'sub_key': 'notification_type',
-            'delimiter': '<br>',
-            'is_optional': True
-        }),
-        ListDyField.data_source('Notification ID', 'data.notification_configurations', options={
-            'sub_key': 'id',
-            'delimiter': '<br>',
-            'is_optional': True
-        }),
-        ListDyField.data_source('Notification ARN', 'data.notification_configurations', options={
-            'sub_key': 'arn',
-            'delimiter': '<br>',
-            'is_optional': True
-        }),
-        TextDyField.data_source('AWS Account ID', 'account', options={
-            'is_optional': True
-        })
+        EnumDyField.data_source(
+            "Access",
+            "data.public_access",
+            default_badge={"indigo.500": ["Private"], "coral.600": ["Public"]},
+        ),
+        TextDyField.data_source("Object Total Counts", "data.object_count"),
+        SizeField.data_source("Object Total Size", "instance_size"),
+        TextDyField.data_source("ARN", "data.arn", options={"is_optional": True}),
+        TextDyField.data_source(
+            "Versioning Status", "data.versioning.status", options={"is_optional": True}
+        ),
+        TextDyField.data_source(
+            "Webhosting Index Document",
+            "data.website_hosting.index_document",
+            options={"is_optional": True},
+        ),
+        TextDyField.data_source(
+            "Webhosting Error Document",
+            "data.website_hosting.error_document",
+            options={"is_optional": True},
+        ),
+        TextDyField.data_source(
+            "Webhosting Error Document",
+            "data.website_hosting.routing_rules",
+            options={"is_optional": True},
+        ),
+        TextDyField.data_source(
+            "Access Logging Target Bucket",
+            "data.server_access_logging.target_bucket",
+            options={"is_optional": True},
+        ),
+        TextDyField.data_source(
+            "Access Logging Target Prefix",
+            "data.server_access_logging.target_prefix",
+            options={"is_optional": True},
+        ),
+        ListDyField.data_source(
+            "Webhosting Routing Rules Condition",
+            "data.website_hosting.routing_rules",
+            options={"sub_key": "condition", "delimiter": "<br>", "is_optional": True},
+        ),
+        TextDyField.data_source(
+            "Object Lock Enabled",
+            "data.object_lock.object_lock_enabled",
+            options={"is_optional": True},
+        ),
+        TextDyField.data_source(
+            "Transfer Acceleration Enabled",
+            "data.transfer_acceleration.transfer_acceleration",
+            options={"is_optional": True},
+        ),
+        ListDyField.data_source(
+            "Notification Type",
+            "data.notification_configurations",
+            options={
+                "sub_key": "notification_type",
+                "delimiter": "<br>",
+                "is_optional": True,
+            },
+        ),
+        ListDyField.data_source(
+            "Notification ID",
+            "data.notification_configurations",
+            options={"sub_key": "id", "delimiter": "<br>", "is_optional": True},
+        ),
+        ListDyField.data_source(
+            "Notification ARN",
+            "data.notification_configurations",
+            options={"sub_key": "arn", "delimiter": "<br>", "is_optional": True},
+        ),
+        TextDyField.data_source(
+            "AWS Account ID", "account", options={"is_optional": True}
+        ),
     ],
     search=[
-        SearchField.set(name='ARN', key='data.arn'),
-        SearchField.set(name='Object Counts', key='data.object_count', data_type='integer'),
-        SearchField.set(name='Object Total Size (Bytes)', key='instance_size', data_type='integer'),
-        SearchField.set(name='AWS Account ID', key='account')
+        SearchField.set(name="ARN", key="data.arn"),
+        SearchField.set(
+            name="Object Counts", key="data.object_count", data_type="integer"
+        ),
+        SearchField.set(
+            name="Object Total Size (Bytes)", key="instance_size", data_type="integer"
+        ),
+        SearchField.set(name="AWS Account ID", key="account"),
+        SearchField.set(
+            name="Access",
+            key="data.public_access",
+            enums={
+                "Private": {"label": "Private", "icon": {"color": "blue.500"}},
+                "Public": {"label": "Public", "icon": {"color": "red.500"}},
+            },
+        ),
     ],
     widget=[
         CardWidget.set(**get_data_from_yaml(bucket_total_count_conf)),
@@ -103,9 +155,9 @@ cst_bucket._metadata = CloudServiceTypeMeta.set_meta(
         ChartWidget.set(**get_data_from_yaml(bucket_count_by_account_conf)),
         ChartWidget.set(**get_data_from_yaml(object_count_by_account_conf)),
         ChartWidget.set(**get_data_from_yaml(object_total_size_by_account_conf)),
-    ]
+    ],
 )
 
 CLOUD_SERVICE_TYPES = [
-    CloudServiceTypeResponse({'resource': cst_bucket}),
+    CloudServiceTypeResponse({"resource": cst_bucket}),
 ]
