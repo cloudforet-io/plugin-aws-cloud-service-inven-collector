6211ad6cdd76298baf957b9aad36a908d4e92f31
mz-ko <hyupko@megazone.com>
2024-08-14 16:34:06 +0900

    fix: Change List field object without sub_key.
    Signed-off-by: mz-ko <hyupko@megazone.com>

--- src/spaceone/inventory/connector/aws_s3_connector/schema/service_type.py
+++ src/spaceone/inventory/connector/aws_s3_connector/schema/service_type.py
@@ -116,22 +116,18 @@ cst_bucket._metadata = CloudServiceTypeMeta.set_meta(
         ),
         ListDyField.data_source(
             "Notification Type",
-            "data.notification_configurations",
-            options={
-                "sub_key": "notification_type",
-                "delimiter": "<br>",
-                "is_optional": True,
-            },
+            "data.notification_configurations.notification_type",
+            options={"delimiter": "<br>", "is_optional": True},
         ),
         ListDyField.data_source(
             "Notification ID",
-            "data.notification_configurations",
-            options={"sub_key": "id", "delimiter": "<br>", "is_optional": True},
+            "data.notification_configurations.id",
+            options={"delimiter": "<br>", "is_optional": True},
         ),
         ListDyField.data_source(
             "Notification ARN",
-            "data.notification_configurations",
-            options={"sub_key": "arn", "delimiter": "<br>", "is_optional": True},
+            "data.notification_configurations.arn",
+            options={"delimiter": "<br>", "is_optional": True},
         ),
         TextDyField.data_source(
             "AWS Account ID", "account", options={"is_optional": True}
