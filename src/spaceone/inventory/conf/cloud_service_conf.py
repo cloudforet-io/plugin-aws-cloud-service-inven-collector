f59d9beea0ea8a1db49330734231cc872b80fc44
jinyoungmoonDEV <moonjinyoung.dev@gmail.com>
2025-01-22 10:22:01 +0900

    fix: add regions option logic

--- src/spaceone/inventory/conf/cloud_service_conf.py
+++ src/spaceone/inventory/conf/cloud_service_conf.py
@@ -50,6 +50,41 @@ CLOUD_SERVICE_GROUP_MAP = {
 
 ASSET_URL = "https://spaceone-custom-assets.s3.ap-northeast-2.amazonaws.com/console-assets/icons/cloud-services/aws"
 
+REGION_LIST = [
+    "us-east-2",
+    "us-east-1",
+    "us-west-1",
+    "us-west-2",
+    "af-south-1",
+    "ap-east-1",
+    "ap-south-2",
+    "ap-southeast-3",
+    "ap-southeast-5",
+    "ap-southeast-4",
+    "ap-south-1",
+    "ap-northeast-3",
+    "ap-northeast-2",
+    "ap-southeast-1",
+    "ap-southeast-2",
+    "ap-northeast-1",
+    "ca-central-1",
+    "ca-west-1",
+    "eu-central-1",
+    "eu-west-1",
+    "eu-west-2",
+    "eu-south-1",
+    "eu-west-3",
+    "eu-south-2",
+    "eu-north-1",
+    "eu-central-2",
+    "il-central-1",
+    "me-south-1",
+    "me-central-1",
+    "sa-east-1",
+    "us-gov-east-1",
+    "us-gov-west-1",
+]
+
 REGION_INFO = {
     "us-east-1": {
         "name": "US East (N. Virginia)",
